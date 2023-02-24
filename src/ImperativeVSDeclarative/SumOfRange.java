1640d6319d7c05bf866abc7573f641a1a3f48c47
Sachin <87515970+sachinredhu16@users.noreply.github.com>
2023-02-24 16:35:16 +0530

    24-FEB-2023

---
+++ src/ImperativeVSDeclarative/SumOfRange.java
@@ -0,0 +1,18 @@
+package ImperativeVSDeclarative;
+
+import java.util.stream.IntStream;
+
+public class SumOfRange {
+    public static void main(String[] args) {
+        System.out.println("================== Imperative ==============");
+        int sum1 =0;
+        for(int i=1;i<100;i++){
+            sum1 +=i;
+        }
+        System.out.println("Sum using Imperative Approach : "+sum1);
+
+        System.out.println("================== Declarative ==============");
+        int sum2 = IntStream.range(1,100).sum();
+        System.out.println("Sum using Declarative Approach : "+sum2);
+    }
+}
